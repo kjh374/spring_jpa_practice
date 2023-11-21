de2c76bb74e7a5c71d6e23f151a9356c2093b14d
JunHyuk <kkjjhh374@naver.com>
2023-11-21 15:58:06 +0900

    #6. JPA 연관관계 매핑(양방향), FETCH JOIN(N+1문제)

--- src/main/java/com/study/jpa/chap04_relation/entity/Employee.java
+++ src/main/java/com/study/jpa/chap04_relation/entity/Employee.java
@@ -5,7 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 
 @Getter @Setter
-@ToString
+@ToString(exclude = "department")
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,7 +22,9 @@ public class Employee {
     @Column(name = "emp_name", nullable = false)
     private String name;
 
-    @ManyToOne
+    // EAGER: 항상 무조건 조인을 수행
+    // LAZY: 필요한 경우에만 조인을 수행. (실무)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dept_id")
     private Department department;
 }
