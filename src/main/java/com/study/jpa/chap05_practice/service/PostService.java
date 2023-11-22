f35454de86a4fbe627c3a98927a8ab26b106816a
JunHyuk <kkjjhh374@naver.com>
2023-11-22 17:30:09 +0900

    #10. 종합 연습예제 완료(UPDATE, DELETE)

--- src/main/java/com/study/jpa/chap05_practice/service/PostService.java
+++ src/main/java/com/study/jpa/chap05_practice/service/PostService.java
@@ -56,9 +56,13 @@ public class PostService {
     }
 
     public PostDetailResponseDTO getDetail(Long id) throws Exception {
-        Post postEntity = postRepository.findById(id).orElseThrow(() -> new RuntimeException(id + "번 게시물이 존재하지 않습니다!"));
+        Post postEntity = getPost(id);
 
         return new PostDetailResponseDTO(postEntity);
+    }
+
+    private Post getPost(Long id) {
+        return postRepository.findById(id).orElseThrow(() -> new RuntimeException(id + "번 게시물이 존재하지 않습니다!"));
     }
 
     public PostDetailResponseDTO insert(PostCreateDTO dto) throws Exception{
@@ -95,5 +99,25 @@ public class PostService {
             });
         }
         return new PostDetailResponseDTO(saved);
+    }
+
+    public PostDetailResponseDTO modify(PostModifyDTO dto) {
+
+        // 수정 전 데이터를 조회
+        Post postEntity = getPost(dto.getPostNo());
+
+        // 수정 시작
+        postEntity.setTitle(dto.getTitle());
+        postEntity.setContent(dto.getContent());
+
+        // 수정 완료
+        Post modifiedPost = postRepository.save(postEntity);
+
+
+        return new PostDetailResponseDTO(modifiedPost);
+    }
+
+    public void delete(Long id) throws Exception{
+        postRepository.deleteById(id);
     }
 }
